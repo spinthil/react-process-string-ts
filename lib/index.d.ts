aada647f89067570bf058b5b571ab44a18b5984d
Thilo Spinner <thilo.spinner@uni-konstanz.de>
2023-03-23 09:29:48 +0100

    Updating package information for publication on NPM.

---
+++ lib/index.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react" />
+/**
+ * This function allows to process strings with regular expressions in React.
+ * @param options Options should be an array of objects containing a regex and a replacement function fn.
+ * @param input The input text in which to replace parts.
+ */
+export type ProcessStringOption = {
+    regex?: RegExp;
+    fn?: (key: number, result: RegExpExecArray) => string | JSX.Element;
+};
+export default function processString(options: ProcessStringOption[]): (input: string | (string | JSX.Element)[]) => string | JSX.Element | (string | JSX.Element)[];
